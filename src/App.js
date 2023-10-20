f6127a562a678f0464164b901c40e90ff3a51eaa
farid <faridramy2003@gmail.com>
2023-10-20 15:42:15 +0300

    added the NotFound404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import NotFound404 from "./pages/NotFound404";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="*" element={<NotFound404 />} />
     </Routes>
   );
 }
