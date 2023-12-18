b8488cd532f67bceedd557fed05bbf8f534422d9
marina <marinarizkallahishak@gmail.com>
2023-12-18 19:44:33 +0200

    final userinfo

--- src/components/UsersInfo.jsx
+++ src/components/UsersInfo.jsx
@@ -75,4 +75,250 @@ const UsersInfo = () => {
     }
   };
 
+  return (
+    <div className="container">
+      <div className="row gutters">
+        <div className="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+          <div className="card h-100">
+            <div className="card-body">
+              <form onSubmit={handleUpdate}>
+                <div className="row">
+                  <div className="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-12">
+                    <div className="form-group">
+                      <label htmlFor="id">ID</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="id"
+                        value={user.id || ""}
+                        disabled
+                      />
+                    </div>
+                  </div>
+                  <div className="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-12">
+                    <div className="form-group">
+                      <label htmlFor="role">Role</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="role"
+                        value={user.role || ""}
+                        disabled
+                      />
+                    </div>
+                  </div>
+                  <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+                    <div className="form-group">
+                      <label htmlFor="createAt">Create at</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="createAt"
+                        value={new Date(user.created_at).toLocaleString() || ""}
+                        disabled
+                      />
+                    </div>
+                  </div>
+                  <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+                    <div className="form-group">
+                      <label htmlFor="createInBranch">Create in branch</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="createInBranch"
+                        value={branch.name || ""}
+                        disabled
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group col-6">
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="firstName"
+                      value={formData.first_name}
+                      onChange={(e) =>
+                        handleInputChange("first_name", e.target.value)
+                      }
+                    />
+                  </div>
+                  <div className="form-group col-6">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="lastName"
+                      value={formData.last_name}
+                      onChange={(e) =>
+                        handleInputChange("last_name", e.target.value)
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group col-6">
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      value={formData.email}
+                      onChange={(e) =>
+                        handleInputChange("email", e.target.value)
+                      }
+                    />
+                  </div>
+                  <div className="form-group col-6">
+                    <label htmlFor="phone">Phone</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="phone"
+                      value={formData.phone}
+                      onChange={(e) => {
+                        const trimmedValue = e.target.value.trim();
+                        /^\d*$/.test(trimmedValue) &&
+                          handleInputChange("phone", trimmedValue);
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group col-6">
+                    <label htmlFor="job">Job</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="job"
+                      value={formData.job}
+                      onChange={(e) => handleInputChange("job", e.target.value)}
+                    />
+                  </div>
+                  <div className="form-group col-6">
+                    <label htmlFor="nationalId">National id</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="nationalId"
+                      value={formData.national_id}
+                      onChange={(e) => {
+                        const trimmedValue = e.target.value.trim();
+                        /^\d*$/.test(trimmedValue) &&
+                          handleInputChange("national_id", trimmedValue);
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group">
+                    <label htmlFor="address">Address</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="address"
+                      value={formData.address}
+                      onChange={(e) =>
+                        handleInputChange("address", e.target.value)
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div className="text-right">
+                      <button type="submit" className="btn btn-success mx-2">
+                        Update
+                      </button>
+                      <button
+                        type="reset"
+                        className="btn btn-secondary"
+                        onClick={() => setReset(!reset)}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <hr className="border-5" />
+              <div className="mb-3">
+                <p className="h2">Subscribed Package :</p>
+                <p className="card-text placeholder-glow">
+                  <span className="placeholder col-7"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-4"></span>
+                  <span className="placeholder col-6"></span>
+                  <span className="placeholder col-8"></span>
+                </p>
+              </div>
+              <div className="mb-3">
+                <p className="h2">History :</p>
+                {history.length > 0 ? (
+                  <table className="table table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Check in</th>
+                        <th scope="col">Check out</th>
+                        <th scope="col">time</th>
+                        <th scope="col">payment</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {history.map((h, index) => {
+                        const checkInTime = new Date(h.check_in_time);
+                        const checkOutTime = new Date(
+                          h.check_out_time ?? h.check_in_time
+                        );
+
+                        const timeDifference = new Date(
+                          checkOutTime - checkInTime
+                        );
+
+                        const hours = timeDifference.getUTCHours();
+                        const minutes = timeDifference.getUTCMinutes();
+                        const seconds = timeDifference.getUTCSeconds();
+
+                        const formattedTimeDifference = `${hours
+                          .toString()
+                          .padStart(2, "0")}:${minutes
+                            .toString()
+                            .padStart(2, "0")}:${seconds
+                              .toString()
+                              .padStart(2, "0")}`;
+
+                        return (
+                          <tr key={index}>
+                            <th scope="row">{index + 1}</th>
+                            <td>{checkInTime.toLocaleString()}</td>
+                            <td>
+                              {checkOutTime.toLocaleString() ===
+                                checkInTime.toLocaleString()
+                                ? "-"
+                                : checkOutTime.toLocaleString()}
+                            </td>
+                            <td>{formattedTimeDifference}</td>
+                            <td>{h.payment ?? "-"}</td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                ) : (
+                  "No history found."
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+
 }
+export default UsersInfo;
+
