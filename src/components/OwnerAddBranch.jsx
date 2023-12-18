02bf01ef807ea1bfe923317775368deb03286dcb
marina <marinarizkallahishak@gmail.com>
2023-12-18 19:32:36 +0200

    handle submit

--- src/components/OwnerAddBranch.jsx
+++ src/components/OwnerAddBranch.jsx
@@ -37,5 +37,50 @@ const AddBranch = (props) => {
   };
 
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.name) return ShowWarningAlert("Please enter the branch name");
+
+    try {
+      const branchResponse = await axios.post(`${url}/branches/create`, {
+        name: formData.name,
+        address: formData.address,
+        phone: formData.phone,
+      });
+
+      if (!branchResponse.data.id)
+        return ShowWarningAlert(
+          branchResponse.data[Object.keys(branchResponse.data)[0]][0]
+        );
+
+      if (
+        formData.workingEmployee &&
+        Object.keys(formData.workingEmployee).length > 0
+      ) {
+        const employeeResponse = await axios.put(
+          `${url}/users/${formData.workingEmployee.id}/update`,
+          {
+            role: formData.workingEmployee.role,
+            first_name: formData.workingEmployee.first_name,
+            last_name: formData.workingEmployee.last_name,
+            branch: branchResponse.data.id,
+          }
+        );
+
+        if (!employeeResponse.data.id)
+          return ShowWarningAlert(
+            employeeResponse.data[Object.keys(employeeResponse.data)[0]][0]
+          );
+      }
+
+      ShowSuccessAlert("Branch added successfully");
+      props.setReRenderNavbar(!props.reRenderNavbar);
+    } catch {
+      ShowWarningAlert("Please check your connection or try again later");
+    }
+  };
+
+
 
 };
