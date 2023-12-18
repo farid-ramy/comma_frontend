a85917dbe9c8c0c1f44c365e737cb60dfd0b47d1
moufid-magdy <moufidmagdy.mm@gmail.com>
2023-12-18 19:33:35 +0200

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -50,4 +50,73 @@ const Login = () => {
       ShowWarningAlert("Please check your connection or try again later");
     }
   };
-}
+
+  // JSX for the login component
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-xl-10 col-lg-12 col-md-9">
+          <div className="card o-hidden border-0 shadow-lg my-5">
+            <div className="card-body p-0">
+              <div className="row">
+                <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                <div className="col-lg-6">
+                  <div className="p-5">
+                    <div className="text-center">
+                      <br />
+                      <h1 className="h3 text-gray-900 mb-4">Welcome Back!</h1>
+                      <br />
+                      <br />
+                      <br />
+                    </div>
+                    <form className="user" onSubmit={handleSubmit}>
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          className="form-control form-control-user"
+                          id="exampleInputUsername"
+                          placeholder="Enter your Username..."
+                          onChange={(e) => handleInputChange(e, setUsername)}
+                        />
+                      </div>
+                      <div className="form-group position-relative">
+                        <input
+                          type={eye ? "password" : "text"}
+                          className="form-control form-control-user"
+                          id="exampleInputPassword"
+                          placeholder="Password"
+                          onChange={(e) => handleInputChange(e, setPassword)}
+                        />
+                        <i
+                          className={`fa-solid position-absolute ${
+                            eye ? "fa-eye" : "fa-eye-slash"
+                          }`}
+                          style={{
+                            right: "20px",
+                            top: "40%",
+                            cursor: "pointer",
+                          }}
+                          onClick={togglePasswordVisibility}
+                        ></i>
+                      </div>
+                      <br />
+                      <br />
+                      <br />
+                      <button className="btn btn-warning btn-user btn-block">
+                        Login
+                      </button>
+                      <br />
+                      <br />
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
